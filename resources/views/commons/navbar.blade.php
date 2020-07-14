f0a5f3925148b230e6559ac0fca6016a56adf032
kazuma.suzuki <kazuma.001600@gmail.com>
2020-07-14 09:57:55 +0000

    navbar

---
+++ resources/views/commons/navbar.blade.php
@@ -0,0 +1,20 @@
+<header class="mb-4">
+    <nav class="navbar navbar-expand-sm navbar-dark bg-danger">
+        {{-- トップページへのリンク --}}
+        <a class="navbar-brand" href="/">RAMEN SHOP</a>
+
+        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#nav-bar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="nav-bar">
+            <ul class="navbar-nav mr-auto"></ul>
+            <ul class="navbar-nav">
+                {{-- ユーザ登録ページへのリンク --}}
+                <li class="nav-item"><a href="#" class="nav-link">会員登録</a></li>
+                {{-- ログインページへのリンク --}}
+                <li class="nav-item"><a href="#" class="nav-link">ログイン</a></li>
+            </ul>
+        </div>
+    </nav>
+</header>
